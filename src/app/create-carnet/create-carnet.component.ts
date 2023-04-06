171547847f3e8f328781b0821526ed0291e160bf
guillaumebeysson <guillaume.beysson@gmail.com>
2023-04-06 23:22:18 +0200

    ajout du drag and drop dans createCarnet

--- src/app/create-carnet/create-carnet.component.ts
+++ src/app/create-carnet/create-carnet.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input, Output, EventEmitter, ElementRef  } from '@angular/core';
+
 
 @Component({
   selector: 'app-create-carnet',
@@ -12,4 +13,44 @@ export class CreateCarnetComponent implements OnInit {
   ngOnInit(): void {
   }
 
+handleFileInput(event: any) {
+  const files = (event.target as HTMLInputElement).files;
+  // Faire quelque chose avec les fichiers ici (par exemple, les envoyer sur un serveur)
+}
+
+imageUrl: string | undefined;
+
+onDragOver(event: DragEvent) {
+  event.preventDefault();
+  this.highlightDropZone(true);
+}
+
+onDragLeave(event: DragEvent) {
+  event.preventDefault();
+  this.highlightDropZone(false);
+}
+
+onDrop(event: DragEvent) {
+  event.preventDefault();
+  this.highlightDropZone(false);
+  
+  const files = event.dataTransfer?.files;
+  if (files && files.length > 0) {
+    const file = files[0];
+    if (file.type.startsWith('image/')) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.imageUrl = reader.result?.toString();
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+}
+
+highlightDropZone(highlight: boolean) {
+  const dropZone = document.querySelector('.drop-zone');
+  if (dropZone) {
+    dropZone.classList.toggle('drag-over', highlight);
+  }
+}
 }
