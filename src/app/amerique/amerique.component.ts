efc9b9a7fa08c539a3eb75067b7e95f894105b92
Beysson Guillaume <guillaume.beysson@gmail.com>
2023-04-07 16:55:45 +0200

    ajout de amerique destinations

--- src/app/amerique/amerique.component.ts
+++ src/app/amerique/amerique.component.ts
@@ -1,26 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+interface CountriesAmerica {
+  name: { common: string };
+  cca2: string;
+  area: string;
+  capital: string[];
+  languages: string[];
+  flags: { svg: string };
+  maps: {googleMaps: string};
+}
+
 @Component({
   selector: 'app-amerique',
   templateUrl: './amerique.component.html',
   styleUrls: ['./amerique.component.css']
 })
+
 export class AmeriqueComponent implements OnInit {
-  countriesAmerica!: any[];
+  countriesData: CountriesAmerica[] | undefined;
 
   constructor(private http: HttpClient) { }
 
-  ngOnInit(): void {
-    this.http.get<any[]>('https://restcountries.com/v3.1/region/americas').subscribe(
-      response => {
-        this.countriesAmerica = response;
-        console.log(this.countriesAmerica);
-      },
-      error => {
-        console.error(error);
-      }
-    );
+  ngOnInit() {
+    this.http.get<CountriesAmerica[]>('https://restcountries.com/v3.1/region/americas').subscribe((data) => {
+      this.countriesData = data;
+      console.log(this.countriesData);
+      console.log()
+    });
   }
   
 
