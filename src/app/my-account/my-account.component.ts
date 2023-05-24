c712b70045ba24874d0207dadf95f31ea06c5d7a
guillaumebeysson <guillaume.beysson@gmail.com>
2023-05-24 11:04:41 +0200

    julie

--- src/app/my-account/my-account.component.ts
+++ src/app/my-account/my-account.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommunicateService } from '../services/communicate.service';
 
 @Component({
   selector: 'app-my-account',
@@ -8,8 +10,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyAccountComponent implements OnInit {
   paysListe: any[] = [];
+  nom: string | null = null
+  title = 'LesCarnetsDeVoyage';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router, private cs: CommunicateService) { }
 
   ngOnInit() {
     //récupère la liste des pays via l'api
@@ -18,6 +22,8 @@ export class MyAccountComponent implements OnInit {
       this.paysListe = paysListe.sort((a, b) => a.translations.fra.common.localeCompare(b.translations.fra.common));
       console.log(paysListe);
     });
+    this.cs.getValue().subscribe(nom => this.nom = nom)
+    console.log("nom....." + this.nom)
   }
 
 }
