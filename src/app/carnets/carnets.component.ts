fc4b6c2a82585f98f024ae4073af7d8775c58f2f
Beysson Guillaume <guillaume.beysson@gmail.com>
2023-05-31 17:01:37 +0200

    add modal suppression

--- src/app/carnets/carnets.component.ts
+++ src/app/carnets/carnets.component.ts
@@ -14,30 +14,54 @@ export class CarnetsComponent implements OnInit {
   carnets?: Carnet[] = [];
   carnet: Carnet = {};
   selectedCarnet?: Carnet;
+  selectedCarnetId: number | undefined;
 
   constructor(private carnetService: CarnetService,private router: Router) { }
 
   ngOnInit(): void {
     this.carnetService.getCarnets().subscribe(res => {
       this.carnets = res;
+      console.log(this.carnets)
     })
   }
 
-  onDelete(id?: number): void {
-    if (id === undefined) {
+  onDelete(): void {
+    if (this.selectedCarnetId === undefined) {
       console.error("L'id est indéfini");
       return;
     }
-    this.carnetService.removeCarnet(id).subscribe({
+    this.carnetService.removeCarnet(this.selectedCarnetId).subscribe({
       next: () => 
         this.carnetService.getCarnets().subscribe(carnets => {
           this.carnets = carnets;
-          console.log("Supression effectuée")
+          console.log("Carnet Supprimé !")
         }), 
       error: () =>
         console.log("Erreur lors de la suppression du carnet")
     });
+
+    this.selectedCarnetId = undefined;
+    this.showModalDelete = false;
   }
+  
+  showModalDelete: boolean = false;
+
+onModal(id?: number) {
+  if (id === undefined) {
+    console.error("L'id est indéfini");
+    return;
+  } else {
+    this.showModalDelete = true;
+    this.selectedCarnetId = id;
+  }
+  console.log(this.showModalDelete)
+  console.log("id..........."+id);
+  
+}
+
+closeModal(){
+  this.showModalDelete = false;
+}
 
 
 }
